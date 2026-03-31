32125c0d31cd172907944192da6cede718c90bfa
cj_ <cjhimanshu49@gmail.com>
2026-03-31 20:25:13 +0530

    solve count triplets with sum smaller than target

--- ds.cpp
+++ ds.cpp
@@ -295,3 +295,36 @@ public:
         }
     }
 };
+
+ 55.Given an array arr[] of distinct integers of size n and a value sum, the task is to find the count of triplets (i, j, k), having (i<j<k) with the sum of (arr[i] + arr[j] + arr[k]) smaller than the given value sum.
+
+Examples :
+
+Input: n = 4, sum = 2, arr[] = {-2, 0, 1, 3}
+Output:  2
+Explanation: Below are triplets with sum less than 2 (-2, 0, 1) and (-2, 0, 3). 
+Input: n = 5, sum = 12, arr[] = {5, 1, 3, 4, 7}
+Output: 4
+Explanation: Below are triplets with sum less than 12 (1, 3, 4), (5, 1, 3), (1, 3, 7) and (5, 1, 4).
+
+solution:-
+
+class Solution {
+  public:
+    long long countTriplets(int n, long long sum, long long arr[]) {
+        sort(arr, arr + n);
+        long long count = 0;
+        for (int i = 0; i < n - 2; i++) {
+            int left = i + 1, right = n - 1;
+            while (left < right) {
+                if (arr[i] + arr[left] + arr[right] < sum) {
+                    count += (right - left);
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return count;
+    }
+};
