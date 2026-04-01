16057e3f7b0ea996914b8b7b14febda16497037d
cj_ <cjhimanshu49@gmail.com>
2026-04-01 23:02:48 +0530

    Happy numbers

--- ds.cpp
+++ ds.cpp
@@ -284,3 +284,42 @@ public:
         }
     }
 };
+
+202. Happy Number
+Easy
+Topics
+premium lock icon
+Companies
+Write an algorithm to determine if a number n is happy.
+
+A happy number is a number defined by the following process:
+
+Starting with any positive integer, replace the number by the sum of the squares of its digits.
+Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+Those numbers for which this process ends in 1 are happy.
+Return true if n is a happy number, and false if not.
+
+solution:
+
+class Solution {
+public:
+    int sumOfSquares(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            sum += d * d;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    bool isHappy(int n) {
+        int slow = n;
+        int fast = sumOfSquares(n);
+        while (fast != 1 && slow != fast) {
+            slow = sumOfSquares(slow);
+            fast = sumOfSquares(sumOfSquares(fast));
+        }
+        return fast == 1;
+    }
+};
